2882c28c6efef2963f8777d328051475ba0dbc09
Ali Hassaan <alihassaan0310@gmail.com>
2020-06-11 16:15:35 +0500

    Completed App

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
+import cx from 'classnames';
+
+import styles from './Cards.module.css';
+
+
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Loading...';
+  }
+
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.infected)}>
+          <CardContent>
+            <Typography variant="h2" color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h3" component="h2">
+              <CountUp start={0} end={confirmed.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="h5" component="p">
+              Number of Infected Cases.
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.recovered)}>
+          <CardContent>
+            <Typography variant= "h2" color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h3" component="h2">
+              <CountUp start={0} end={recovered.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="h5" component="p">
+              Number of recovered cases.
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.deaths)}>
+          <CardContent>
+            <Typography variant="h2" color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h3" component="h2">
+              <CountUp start={0} end={deaths.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="h5" component="p">
+              Number of deaths.
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.RecoveryRate)}>
+                <CardContent>
+                    <Typography variant="h2" color="textSecondary">
+                      Recovery Rate
+                      </Typography>
+                    <Typography variant="h3">
+                        {Math.round(recovered.value/confirmed.value*100)}%
+                    </Typography>
+                </CardContent>
+            </Grid>
+            <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.DeathRate)}>
+          <CardContent>
+                    <Typography variant="h2" color="textSecondary">
+                      Death Rate</Typography>
+                    <Typography variant="h3">
+                        {Math.round(deaths.value/confirmed.value*100)}%
+                    </Typography>
+                </CardContent>   
+            </Grid>
+</Grid>
+    </div>
+  );
+};
+export default Info;
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,132 @@
+import React, { useState, useEffect, } from 'react';
+import { Line, Bar, Polar} from 'react-chartjs-2';
+
+import { fetchDailyData } from '../../api';
+
+import styles from './Chart.module.css';
+
+
+
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchMyAPI = async () => {
+      const initialDailyData = await fetchDailyData();
+
+      setDailyData(initialDailyData);
+    };
+
+    fetchMyAPI();
+  }, []);
+
+  const barChart = (
+    confirmed ? (
+      <Bar
+        data={{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              hoverBackgroundColor: ['rgba(0, 0, 255)', 'rgba(0, 255, 0)', 'rgba(255, 0, 0)'],
+              hoverBorderWidth: 50,
+              data: [confirmed.value, recovered.value, deaths.value],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state in ${country}` },
+        }}
+      />
+    ) : null
+  );
+
+  const lineChart = (
+    dailyData[0] ? (
+      <Line
+        data={{
+          labels: dailyData.map(({ date }) => date),
+          datasets: [{
+            data: dailyData.map((data) => data.confirmed),
+            label: 'Infected',
+            borderColor: 'rgba(0, 255, 0, 0.5)',
+            fill: true,
+          }, {
+            data: dailyData.map((data) => data.deaths),
+            label: 'Deaths',
+            borderColor: 'rgba(255, 0, 0, 0.5)',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true,
+          },
+          ],
+        }}
+      />
+    ) : null
+  );
+
+  if(!confirmed) {
+    return "..loading";
+}
+//console.log(confirmed.value);
+let confirmedValue = confirmed.value;
+let recoveredValue = recovered.value;
+let deathsValue = deaths.value;
+const polarChart = (
+    
+    <Polar 
+        data={{
+            labels: ['Confirmed', 'Recovered', 'Deaths'],
+            datasets: [
+                {
+                    data: [confirmedValue, recoveredValue, deathsValue],
+                    backgroundColor: [
+                        'rgb(0, 0, 255, 0.5)',
+                        'rgb(0, 255, 0, 0.5)',
+                        'rgb(255, 0, 0, 0.5)',
+                    ], 
+                    borderColor: ['rgb(0, 0, 0)',
+                    'rgb(0, 0, 0)',
+                    'rgb(0, 0, 0)',],
+                    hoverBackgroundColor: ['rgb(0, 0, 255)',
+                    'rgb(0, 255, 0)',
+                    'rgb(255, 0, 0)',],
+                    hoverBorderWidth: 3,
+                }
+            ]
+        }}
+        options={{
+            title: { display: true, text: `(Infected vs Recovered vs Deaths) ${country}`},
+            plugins: {
+                labels: [
+                    {
+                        render: 'percentage',
+                        precision: 0,
+                        position: 'outside'
+                    }
+                ]
+            }
+        }}
+    />
+);
+
+
+
+
+  return (
+    <div className={styles.container}>
+      {country ? barChart : lineChart}
+      <div className={styles.polarChartClass}>
+                    {polarChart}
+                </div>
+     </div>
+  );
+};
+
+
+
+
+
+export default Chart;
